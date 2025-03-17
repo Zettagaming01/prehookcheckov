eaf301499941e9bb58a2811deb0095a4619ce1d1
Vince <negapatanvincejay@gmail.com>
2025-03-17 23:52:37 +0800

    added storage 6

--- main.tf
+++ main.tf
@@ -16,40 +16,8 @@ resource "azurerm_storage_account" "example" {
     private_service_connection {
       name                       = "example-privateserviceconnection"
       is_manual_connection      = false
-      private_connection_resource_id = azurerm_private_endpoint.example.id
+      private_connection_resource_id = azurerm_storage_account.example.id
       subresource_names          = ["blob"]
     }
   }
-}
-
-# Private endpoint resource
-resource "azurerm_private_endpoint" "example" {
-  name                = "example-endpoint"
-  location            = "West Europe"
-  resource_group_name = "example-rg"
-  subnet_id           = azurerm_subnet.example.id
-
-  private_service_connection {
-    name                           = "example-privateserviceconnection"
-    private_connection_resource_id = azurerm_storage_account.example.id
-    is_manual_connection          = false
-    subresource_names             = ["blob"]
-  }
-}
-
-# Required networking resources
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "West Europe"
-  resource_group_name = "example-rg"
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = "example-rg"
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-  private_endpoint_network_policies_enabled = true
 }
